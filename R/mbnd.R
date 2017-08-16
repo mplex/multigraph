d35a20694aca1ea49828318bb4a463a2bfb562bc
Antonio Rivero Ostoic <multiplex@post.com>
2017-08-16 11:07:47 +0200

    v07 beta

--- R/mbnd.R
+++ R/mbnd.R
@@ -1,6 +1,6 @@
 mbnd <-
-function (pares, r, b, vlt, cx, lwd, ecol, directed, asp, bwd, 
-    alfa, fds, flgcx, weighted) 
+function (pares, r, b, vlt, cx, lwd, ecol, directed, bw, alfa, 
+    fds, flgcx, weighted, flgcr, hds, n) 
 {
     ifelse(isTRUE(nrow(pares) > 2L) == TRUE, pares <- pares[c(1, 
         nrow(pares)), ], NA)
@@ -40,29 +40,40 @@ function (pares, r, b, vlt, cx, lwd, ecol, directed, asp, bwd,
         ifelse(isTRUE(gx >= 90L) == TRUE, agx <- abs(90L - (gx%%90L)), 
             agx <- gx%%90L)
     }
+    if (isTRUE(weighted == FALSE) == TRUE) {
+        if (isTRUE(mean(lwd) < 6L) == TRUE) {
+            fds <- fds - (mean(lwd) * 4)
+        }
+        else if (isTRUE(mean(lwd) > 15L) == TRUE) {
+            fds <- fds - (mean(lwd) * 2)
+        }
+        else {
+            fds <- fds - (mean(lwd) * 2.5)
+        }
+    }
     ifelse(isTRUE(weighted == TRUE) == TRUE && isTRUE(mean(cx) < 
         2L) == TRUE, cx <- cx + (2L - mean(cx)), NA)
-    d <- (rng(r) * ((bwd * 1000L) * ((2L^(abs(sin(angp * (pi/180L)))))/1200L)) * 
+    d <- (rng(r) * ((bw * 1000L) * ((2L^(abs(sin(angp * (pi/180L)))))/1200L)) * 
         (mean(cx)/2L)) * ((30L + (agx * (6L/45L) * -1L))/100L)
     orott <- orot
     if (isTRUE(flgcx == TRUE) == TRUE) {
-        orott[1, 1] <- (cx[1]/fds) - orot[1, 1]
+        orott[1, 1] <- (cx[1]/(fds - 0)) - orot[1, 1]
         orott[2, 1] <- orot[2, 1] - (cx[2]/(fds))
     }
     else if (isTRUE(flgcx == FALSE) == TRUE) {
         ifelse(isTRUE(weighted == TRUE) == TRUE && isTRUE(mean(lwd) > 
             2L) == TRUE, lc <- (max(lwd) * 0.1), lc <- 0)
-        orott[1, 1] <- ((cx[1] + lc)/fds) - orot[1, 1]
+        orott[1, 1] <- ((cx[1] + lc)/(fds - 0)) - orot[1, 1]
         orott[2, 1] <- orot[2, 1] - ((cx[1] + lc)/fds)
     }
     lst <- array(0L, dim = c(2, 2, r))
     dat <- data.frame(matrix(nrow = 0L, ncol = 2L))
-    for (i in 1:r) {
-        lst[, 1, i] <- orott[, 1]
-        lst[, 2, i] <- d[i]
-        dat[(nrow(dat) + 1L):(nrow(dat) + 2L), ] <- lst[, , i]
+    for (j in 1:r) {
+        lst[, 1, j] <- orott[, 1]
+        lst[, 2, j] <- d[j]
+        dat[(nrow(dat) + 1L):(nrow(dat) + 2L), ] <- lst[, , j]
     }
-    rm(i)
+    rm(j)
     rrot <- xyrt(dat, as.numeric(angp))
     if (isTRUE(pares[1, 1] != 0L | pares[1, 2] != 0L) == TRUE) {
         rrot[, 1] <- rrot[, 1] - xo
@@ -72,73 +83,100 @@ function (pares, r, b, vlt, cx, lwd, ecol, directed, asp, bwd,
         if (isTRUE(weighted == TRUE) == TRUE && isTRUE(directed == 
             TRUE) == TRUE) {
             if (isTRUE(b[i] %in% multiplex::men(b)[1]) == TRUE) {
-                graphics::arrows(rrot[which(seq(1:nrow(dat))%%2L == 
-                  1L)[i], 1], rrot[which(seq(1:nrow(dat))%%2L == 
-                  1L)[i], 2], rrot[which(seq(1:nrow(dat))%%2L == 
-                  0L)[i], 1], rrot[which(seq(1:nrow(dat))%%2L == 
+                graphics::arrows(rrot[which(seq_len(nrow(dat))%%2L == 
+                  1L)[i], 1], rrot[which(seq_len(nrow(dat))%%2L == 
+                  1L)[i], 2], rrot[which(seq_len(nrow(dat))%%2L == 
+                  0L)[i], 1], rrot[which(seq_len(nrow(dat))%%2L == 
                   0L)[i], 2], code = 2, length = 0, angle = 0, 
                   lty = vlt[i], lwd = lwd[i], col = grDevices::adjustcolor(ecol[i], 
                     alpha = alfa))
             }
             else if (isTRUE(b[i] %in% multiplex::men(b)[1]) == 
                 FALSE) {
-                graphics::arrows(rrot[which(seq(1:nrow(dat))%%2L == 
-                  1L)[i], 1], rrot[which(seq(1:nrow(dat))%%2L == 
-                  1L)[i], 2], rrot[which(seq(1:nrow(dat))%%2L == 
-                  0L)[i], 1], rrot[which(seq(1:nrow(dat))%%2L == 
+                graphics::arrows(rrot[which(seq_len(nrow(dat))%%2L == 
+                  1L)[i], 1], rrot[which(seq_len(nrow(dat))%%2L == 
+                  1L)[i], 2], rrot[which(seq_len(nrow(dat))%%2L == 
+                  0L)[i], 1], rrot[which(seq_len(nrow(dat))%%2L == 
                   0L)[i], 2], code = 1, length = 0, angle = 0, 
                   lty = vlt[i], lwd = lwd[i], col = grDevices::adjustcolor(ecol[i], 
                     alpha = alfa))
             }
         }
         else {
-            graphics::segments(rrot[which(seq(1:nrow(dat))%%2L == 
-                1L)[i], 1], rrot[which(seq(1:nrow(dat))%%2L == 
-                1L)[i], 2], rrot[which(seq(1:nrow(dat))%%2L == 
-                0L)[i], 1], rrot[which(seq(1:nrow(dat))%%2L == 
+            graphics::segments(rrot[which(seq_len(nrow(dat))%%2L == 
+                1L)[i], 1], rrot[which(seq_len(nrow(dat))%%2L == 
+                1L)[i], 2], rrot[which(seq_len(nrow(dat))%%2L == 
+                0L)[i], 1], rrot[which(seq_len(nrow(dat))%%2L == 
                 0L)[i], 2], lty = vlt[i], lwd = lwd[i], col = grDevices::adjustcolor(ecol[i], 
                 alpha = alfa))
         }
         if (isTRUE(directed == TRUE) == TRUE) {
+            if (isTRUE(n < 15) == TRUE) {
+                Hd <- data.frame(x = c((-0.6 - (0.01538462 * 
+                  n)), (-0.35 - (0.01538462 * n)), (-0.6 - (0.01538462 * 
+                  n)), (0.4 - (0.01538462 * n))), y = c(-0.5, 
+                  0, 0.5, 0)) * (hds)
+            }
+            else {
+                Hd <- data.frame(x = c(-0.7, -0.45, -0.7, 0.3), 
+                  y = c(-0.5, 0, 0.5, 0)) * (hds)
+            }
             if (isTRUE(as.numeric(lwd[i]) < 7L) == TRUE) {
-                Hd <- data.frame(x = c(-0.8, -0.55, -0.8, 0.2), 
-                  y = c(-0.5, 0, 0.5, 0))
                 ifelse(isTRUE(as.numeric(lwd[i]) <= 1L) == TRUE, 
                   Hd <- Hd * (as.numeric(lwd[i]))/((as.numeric(lwd[i]) * 
-                    8.571) + 41.429), Hd <- Hd * (as.numeric(lwd[i]))/((as.numeric(lwd[i]) * 
-                    8.571) + 51.429))
+                    8.571) + 30), Hd <- Hd * (as.numeric(lwd[i]))/((as.numeric(lwd[i]) * 
+                    8.571) + 40))
             }
-            else if (isTRUE(as.numeric(lwd[i]) >= 10L) == TRUE) {
-                Hd <- data.frame(x = c(-0.7, -0.45, -0.7, 0.3), 
-                  y = c(-0.5, 0, 0.5, 0))
+            else if (isTRUE(as.numeric(lwd[i]) >= 15L) == TRUE) {
                 Hd <- Hd * (as.numeric(lwd[i]))/(as.numeric(lwd[i]) + 
                   120L)
             }
             else {
-                Hd <- data.frame(x = c(-0.75, -0.5, -0.75, 0.25), 
-                  y = c(-0.5, 0, 0.5, 0))
                 Hd <- Hd * (as.numeric(lwd[i]))/((as.numeric(lwd[i]) * 
-                  8.571) + 51.429)
+                  8.571) + 40)
             }
-            if (isTRUE(b[i] %in% multiplex::men(b)[1]) == FALSE) {
-                prx <- rrot[which(seq(1:nrow(dat))%%2L == 1L)[i], 
-                  1]
-                pry <- rrot[which(seq(1:nrow(dat))%%2L == 1L)[i], 
-                  2]
-                hd <- xyrt((Hd), (as.numeric(angp) - 180L))
+            if (isTRUE(i %in% flgcr) == TRUE) {
+                prx1 <- rrot[which(seq_len(nrow(dat))%%2L == 
+                  1L)[i], 1]
+                pry1 <- rrot[which(seq_len(nrow(dat))%%2L == 
+                  1L)[i], 2]
+                hd1 <- xyrt((Hd), (as.numeric(angp) - 180L))
+                hd1[, 1] <- hd1[, 1] + prx1
+                hd1[, 2] <- hd1[, 2] + pry1
+                graphics::polygon((hd1), col = grDevices::adjustcolor(ecol[i], 
+                  alpha = alfa), border = NA)
+                prx2 <- rrot[which(seq_len(nrow(dat))%%2L == 
+                  0L)[i], 1]
+                pry2 <- rrot[which(seq_len(nrow(dat))%%2L == 
+                  0L)[i], 2]
+                hd2 <- xyrt(Hd, (as.numeric(angp) - 0L))
+                hd2[, 1] <- hd2[, 1] + prx2
+                hd2[, 2] <- hd2[, 2] + pry2
+                graphics::polygon((hd2), col = grDevices::adjustcolor(ecol[i], 
+                  alpha = alfa), border = NA)
             }
-            else if (isTRUE(b[i] %in% multiplex::men(b)[1]) == 
-                TRUE) {
-                prx <- rrot[which(seq(1:nrow(dat))%%2L == 0L)[i], 
-                  1]
-                pry <- rrot[which(seq(1:nrow(dat))%%2L == 0L)[i], 
-                  2]
-                hd <- xyrt(Hd, (as.numeric(angp) - 0L))
+            else {
+                if (isTRUE(b[i] %in% multiplex::men(b)[1]) == 
+                  FALSE) {
+                  prx <- rrot[which(seq_len(nrow(dat))%%2L == 
+                    1L)[i], 1]
+                  pry <- rrot[which(seq_len(nrow(dat))%%2L == 
+                    1L)[i], 2]
+                  hd <- xyrt((Hd), (as.numeric(angp) - 180L))
+                }
+                else if (isTRUE(b[i] %in% multiplex::men(b)[1]) == 
+                  TRUE) {
+                  prx <- rrot[which(seq_len(nrow(dat))%%2L == 
+                    0L)[i], 1]
+                  pry <- rrot[which(seq_len(nrow(dat))%%2L == 
+                    0L)[i], 2]
+                  hd <- xyrt(Hd, (as.numeric(angp) - 0L))
+                }
+                hd[, 1] <- hd[, 1] + prx
+                hd[, 2] <- hd[, 2] + pry
+                graphics::polygon((hd), col = grDevices::adjustcolor(ecol[i], 
+                  alpha = alfa), border = NA)
             }
-            hd[, 1] <- hd[, 1] + prx
-            hd[, 2] <- hd[, 2] + pry
-            graphics::polygon((hd), col = grDevices::adjustcolor(ecol[i], 
-                alpha = alfa), border = NA)
         }
         else {
             NA
--- R/stsm.R
+++ R/stsm.R
@@ -7,6 +7,8 @@ function (net, seed = seed, maxiter = 40, drp, jitter, method,
         NA, drp <- 0)
     netd <- multiplex::mnplx(replace(net, net <= drp, 0), directed = FALSE, 
         dichot = TRUE)
+    ifelse(is.null(rownames(netd)) == TRUE, lbs <- seq_len(n), 
+        lbs <- rownames(netd))
     ifelse(missing(method) == TRUE, delta <- stats::dist(netd, 
         upper = TRUE, diag = TRUE, method = "binary"), delta <- stats::dist(netd, 
         upper = TRUE, diag = TRUE, method = method))
@@ -15,8 +17,8 @@ function (net, seed = seed, maxiter = 40, drp, jitter, method,
     mwd <- delta^(-2)
     mwd[which(mwd == Inf)] <- 0
     lpmwd <- as.matrix(mwd)
-    for (i in 1:n) {
-        for (j in 1:n) {
+    for (i in seq_len(n)) {
+        for (j in seq_len(n)) {
             ifelse(isTRUE(as.matrix(mwd)[i, j] != 0) == TRUE, 
                 lpmwd[i, j] <- -1 * as.matrix(mwd)[i, j], NA)
         }
@@ -38,7 +40,7 @@ function (net, seed = seed, maxiter = 40, drp, jitter, method,
     }
     set.seed(seed)
     vec <- stats::rnorm(n * 2L)
-    X0 <- cbind(vec[1:n], vec[(n + 1):length(vec)])
+    X0 <- cbind(vec[seq_len(n)], vec[(n + 1):length(vec)])
     Xs <- as.matrix(X0)
     newstss <- sts(X0, delta = delta, mwd = mwd)
     for (iter in 1:maxiter) {
@@ -59,31 +61,58 @@ function (net, seed = seed, maxiter = 40, drp, jitter, method,
         }
     }
     rm(iter)
-    if (isTRUE(length(multiplex::comps(netd)$isol) > 2) == TRUE) {
-        isol <- which(duplicated(round(Xs, 3)) | duplicated(round(Xs, 
-            3), fromLast = TRUE))
-        tmpi <- popl(length(isol), seed = seed) * (1/length(isol) * 
-            length(isol))
-        locx <- ((tmpi[, 1]/3L) - (min(Xs[, 1])) - 0)
-        ifelse(isTRUE((max(Xs[, 1]) - min(Xs[, 1]))/(max(Xs[, 
-            2]) - min(Xs[, 2])) > 0) == TRUE, locy <- ((min(Xs[, 
-            2])) - (tmpi[, 2]/3L) - 0), locy <- ((max(Xs[, 2])) + 
-            (tmpi[, 2]/3L) + 0))
-        Xs.chull <- grDevices::chull(Xs)
-        Xs.chull <- Xs[Xs.chull, ]
-        ifelse(isTRUE(length(which(Xs.chull[, 1] < mean(Xs.chull[, 
-            1]))) > length(which(Xs.chull[, 1] > mean(Xs.chull[, 
-            1])))) == TRUE, locx <- locx + (2L/10L), locx <- locx + 
-            ((2L/10L) * -1))
-        ifelse(isTRUE(length(which(Xs.chull[, 2] < mean(Xs.chull[, 
-            2]))) > length(which(Xs.chull[, 2] > mean(Xs.chull[, 
-            2])))) == TRUE, locy <- locy - (2L/10L), locy <- locy - 
-            ((2L/10L) * -1))
-        Xs[isol, ] <- (cbind(locx, locy))
+    cmps <- multiplex::comps(netd)
+    nds <- Xs
+    ifelse(isTRUE(sum(nds) == 0) == TRUE, rat <- 1, rat <- (max(nds[, 
+        1]) - min(nds[, 1]))/(max(nds[, 2]) - min(nds[, 2])))
+    if (isTRUE(length(cmps$isol) > 1) == TRUE) {
+        nds[, 1] <- (nds[, 1] - min(nds[, 1]))/(max(nds[, 1]) - 
+            min(nds[, 1]))
+        ifelse(isTRUE(rat > 0) == TRUE, nds[, 2] <- ((nds[, 2] - 
+            min(nds[, 2]))/(max(nds[, 2]) - min(nds[, 2]))) * 
+            (1L/rat), nds[, 2] <- ((nds[, 2] - min(nds[, 2]))/(max(nds[, 
+            2]) - min(nds[, 2]))) * (rat))
+        nds <- as.matrix((nds))
+        ndst <- nds[which(nds[, 1] != 0), ]
+        tmpi <- popl(length(cmps$isol), seed = seed)/(length(cmps$isol) * 
+            2) * length(cmps$isol)
+        if (is.null(cmps$com) == FALSE) {
+            locx <- ((tmpi[, 1]/3L) - (min(ndst[, 1])) - 0)
+            ifelse(isTRUE(rat > 0) == TRUE, locy <- ((min(ndst[, 
+                2])) - (tmpi[, 2]/3L) - 0), locy <- ((max(ndst[, 
+                2])) + (tmpi[, 2]/3L) + 0))
+            ndst.chull <- grDevices::chull(ndst)
+            ndst.chull <- ndst[ndst.chull, ]
+            ifelse(isTRUE(length(which(ndst.chull[, 1] < mean(ndst.chull[, 
+                1]))) > length(which(ndst.chull[, 1] > mean(ndst.chull[, 
+                1])))) == TRUE, locx <- locx + (1/n), locx <- locx + 
+                ((1/n) * -1))
+            ifelse(isTRUE(length(which(ndst.chull[, 2] < mean(ndst.chull[, 
+                2]))) > length(which(ndst.chull[, 2] > mean(ndst.chull[, 
+                2])))) == TRUE, locy <- locy - (1/n), locy <- locy - 
+                ((1/n) * -1))
+        }
+        else {
+            locx <- (tmpi[, 1])
+            locy <- (tmpi[, 2])
+        }
+        nds[which(lbs %in% cmps$isol), ] <- (cbind(locx, locy))
     }
-    else {
-        NA
+    else if (isTRUE(length(cmps$isol) == 1L) == TRUE) {
+        ndst <- nds[which(nds[, 1] != 0), ]
+        locx <- max(ndst[, 1]) + (1/n)
+        ifelse(isTRUE(rat < 0) == TRUE, locy <- max(ndst[, 2]) + 
+            (1/n), locy <- min(ndst[, 2]) - (1/n))
+        nds[which(lbs %in% cmps$isol), ] <- (cbind(locx, locy))
     }
+    nds[, 1] <- (nds[, 1] - min(nds[, 1]))/(max(nds[, 1]) - min(nds[, 
+        1])) + (1/n)
+    ifelse(isTRUE(rat > 0) == TRUE, nds[, 2] <- ((nds[, 2] - 
+        min(nds[, 2]))/(max(nds[, 2]) - min(nds[, 2]))) * (1L/rat), 
+        nds[, 2] <- ((nds[, 2] - min(nds[, 2]))/(max(nds[, 2]) - 
+            min(nds[, 2]))) * (rat) + (1/n))
+    nds[, 2] <- nds[, 2] * -1
+    Xs <- as.data.frame(nds)
     if (missing(jitter) == FALSE && isTRUE(jitter) == TRUE) {
         jitter(Xs, amount = (n/100))
     }
