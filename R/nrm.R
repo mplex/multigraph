92f863db1fe9f22e6cce26bcdccfa335e9a91bdc
Antonio Rivero Ostoic <multiplex@post.com>
2017-08-17 14:33:45 +0200

    v07 beta

--- R/nrm.R
+++ R/nrm.R
@@ -1,8 +1,7 @@
 nrm <-
 function (x, digits = 3) 
 {
-    if (isTRUE(length(x) == 1L) == TRUE | isTRUE(sum(x) == 0) == 
-        TRUE) 
+    if (isTRUE(length(x) == 1L) == TRUE) 
         return(x)
     if (is.array(x) == TRUE) {
         xnorm <- (x[, 1] - min(x[, 1]))/(max(x[, 1]) - min(x[, 
